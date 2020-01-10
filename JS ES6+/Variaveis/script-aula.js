06b17d0a42ecd74c62021ac698a8e038b9603b9c
Gabriel <bielpini@yahoo.com.br>
2020-01-10 14:48:13 -0300

    Aula 02020 do curso de javascript

---
+++ JS ES6+/Variaveis/script-aula.js
@@ -0,0 +1,12 @@
+var nome = "Gabriel";
+var idade = 19;
+var ehEstudande = true;
+
+console.log(nome,idade,ehEstudande,nome);
+
+var preco = 25,
+    totalComprado = 7,
+    totalPago =  preco * totalComprado;
+
+    console.log(totalPago);
+
---
+++ JS ES6+/Variaveis/script.js
@@ -0,0 +1,19 @@
+//Variavevl string
+
+var nome = "Gabriel";
+console.log(nome);
+
+//variavel numerica
+
+var idade = 19;
+console.log(idade);
+
+//variavel indefinida
+
+var bandaFavorita;
+console.log(bandaFavorita);
+
+//atribuicao do valor
+
+bandaFavorita =  "S.O.A.D."
+console.log(bandaFavorita);
